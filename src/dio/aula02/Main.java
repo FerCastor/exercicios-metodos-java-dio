033f085bc14f9bbcc217708a71196e4ba51e0d7d
FerCastor <fernandacastor0@gmail.com>
2022-05-16 13:45:10 -0300

    adiciona teste de losango

--- src/dio/aula02/Main.java
+++ src/dio/aula02/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
     Quadrilatero.area(3);
-    Quadrilatero.area(5, 5);
+    Quadrilatero.area(5d, 5d);
     Quadrilatero.area(7, 8, 9);
+    Quadrilatero.area(5f,5f);
     }
 }
--- src/dio/aula02/Quadrilatero.java
+++ src/dio/aula02/Quadrilatero.java
@@ -13,4 +13,7 @@ public class Quadrilatero {
     public static void area(double baseMaior, double baseMenor, double altura) {
         System.out.println("Área do trapézio: " + ((baseMaior+baseMenor)*altura) /2);
     }
+    public static void area(float diagonal1, float diagonal2) {
+        System.out.println("Área do losango: " + (diagonal1 * diagonal1)/2);
+    }
 }
